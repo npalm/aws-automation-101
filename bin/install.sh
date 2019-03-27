e22593b82f1c555f387a06ed87bda48a2854be50
Niek Palm <dev.npalm@gmail.com>
2019-03-27 21:52:02 +0100

    Add install script

---
+++ bin/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+TERRAFORM_VERSION=0.11.13
+BIN_DIR=/home/ec2-user/.local/bin
+
+mkdir tmp
+cd tmp
+
+wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+
+
+# crete bin dir
+mkdir -p ${HOME}/bin
+mv terraform ${BIN_DIR}
+
+cd ..
+rm -rf tmp
+
+echo Terraform $(terraform --version) is installed
